fbb68a95fbb0ea31a305834f1c0895614a534870
aonatsu-admin <senahoribe480@gmail.com>
2025-11-02 12:50:40 +0900

    C++で視界内にプレイヤーがいるときに追跡する処理を実装，パトロールやBGMは未実装

--- Source/VRProject/ChaserAIController.cpp
+++ Source/VRProject/ChaserAIController.cpp
@@ -1,77 +1,42 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "ChaserAIController.h"
+#include "Kismet/GameplayStatics.h"  //GetPlayerPawn（Get Player Character)のために追加
 
-//#include "ChaserAIController.h"
-//#include "Kismet/GameplayStatics.h"
-//#include "GameFramework/Character.h"
+void AChaserAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
 
-//起動時に一度だけ呼ばれる関数
-//void AChaserAIController::BeginPlay()
-//{
-//	Super::BeginPlay();
-//	// プレイヤーへのポインタを取得して変数に格納
-//	APawn* VRPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-//
-//	//if (VRPawn)
-//	//{
-//	//	//Chaserを特定の位置にテレポートさせる
-//	//	APawn* ChaserPawn = GetPawn();
-//	//	if (ChaserPawn)
-//	//	{
-//	//		FVector SpawnLocation = FVector(1000.f, 1000.f, 100.f); // 例: (1000, 1000, 100)の位置にスポーン
-//	//		FRotator SpawnRotation = FRotator::ZeroRotator; // 回転はゼロ
-//	//		ChaserPawn->SetActorLocationAndRotation(SpawnLocation, SpawnRotation);
-//	//	}
-//	//}
-//}
+	//プレイヤーキャラクターの取得（Get Player Characterノード）
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-// 毎フレーム呼ばれる関数
-//void AChaserAIController::Tick(float DeltaSeconds)
-//{
-//	Super::Tick(DeltaSeconds);
-//	
-//	//プレイヤーを見つけるまで一定の速度で既定のルートを移動
-//	APawn* VRPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-//	ACharacter* ChaserCharacter = Cast<ACharacter>(GetPawn());
-//
-//	//ChaserCharacterが存在しない場合は処理を中断
-//	if (!ChaserCharacter) return;
-//
-//	//視界判定
-//	bool bCanSeePlayer = VRPawn && LineOfSightTo(VRPawn);
-//
-//	if (bCanSeePlayer)
-//	{
-//		//追跡時の速度
-//		ChaserCharacter->GetCharacterMovement()->MaxWalkSpeed = 600.0f; // 走る速度に設定
-//		MoveToActor(VRPawn, 5.0f); // 5.0fは停止距離
-//	}
-//	else
-//	{
-//		//徘徊時の測度
-//		ChaserCharacter->GetCharacterMovement()->MaxWalkSpeed = 300.0f; // 歩く速度に設定
-//
-//		//パトロールポイントへ移動
-//		if (PatrolPoints.Num() > 0)
-//		{
-//			//現在のパトロールポイントに移動
-//			MoveToLocation(PatrolPoints[CurrentPatrolPointIndex], 5.0f); // 5.0fは停止距離
-//
-//			//パトロールポイントに到達したかどうかをチェック
-//			if (FVector::Dist(ChaserCharacter->GetActorLocation(), PatrolPoints[CurrentPatrolPointIndex]) < 50.0f) // 50.0fは到達距離の閾値
-//			{
-//				//次のパトロールポイントに切り替え
-//				CurrentPatrolPointIndex = (CurrentPatrolPointIndex + 1) % PatrolPoints.Num();
-//			}
-//		}
-//	}
-//
-//	//プレイヤーを見つけたら追跡
-//	if (VRPawn)
-//	{
-//		MoveToActor(VRPawn, 5.0f); // 5.0fは停止距離
-//	}
-//	
-//}
+	if (PlayerPawn)
+	{
+		bool bCanSeePlayer = LineOfSightTo(PlayerPawn);  //Chaserの視界内にプレイヤーがいるかどうかを確認
+
+		if (bCanSeePlayer)
+		{
+			if (bIsChasing == false)
+			{
+				bIsChasing = true;
+				OnPlalyerSpotted();  //プレイヤーを発見したときのイベントを呼び出す
+			}
+
+			//プレイヤーを追跡
+			MoveToActor(PlayerPawn, 5.0f);  //5.0fは停止距離
+		}
+		else
+		{
+			if (bIsChasing == true)
+			{
+				bIsChasing = false;
+				OnPlayerLost();  //プレイヤーを見失ったときのイベントを呼び出す
+			}
+
+			//プレイヤーを見失った場合の処理（例：停止）
+			StopMovement();
+		}
+	}
+}
 
 
--- Source/VRProject/ChaserAIController.h
+++ Source/VRProject/ChaserAIController.h
@@ -1,25 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-//#pragma once
-//
-//#include "CoreMinimal.h"
-//#include "AIController.h"
-//#include "ChaserAIController.generated.h"
+#pragma once
 
-/**
- * 
- */
-//UCLASS()
-//class VRPROJECT_API AChaserAIController : public AAIController
-//{
-//	GENERATED_BODY()
-//
-//protected:
-//	virtual void BeginPlay() override;
-//	//パトロールポイント
-//	UPROPERTY(EditAnywhere, BlueprintReadWrite = "AI")
-//	TArray<AActor*> PatrolPoints;
-//
-//	//現在のパトロールポイントのインデックス
-//	int32 CurrentPatrolPointIndex = 0;
-//};
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ChaserAIController.generated.h"
+
+UCLASS()
+class VRPROJECT_API AChaserAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	//Chaserの視界内にプレイヤーが存在するかどうかを記憶する変数
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	bool bIsChasing;
+
+	//C++から呼び出す，ブループリント側で実装されるイベント
+	UFUNCTION(BlueprintImplementableEvent, Category = "AI")
+	void OnPlalyerSpotted();  //プレイヤーを発見したときに呼び出されるイベント
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "AI")
+	void OnPlayerLost();  //プレイヤーを見失ったときに呼び出されるイベント
+
+};
